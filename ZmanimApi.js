9438f82bf3a371e748ee09a88e52318f97a56ee2
Moshe Stern <guntheramassed@gmail.com>
2023-07-14 12:15:44 -0400

    shabbos

--- ZmanimApi.js
+++ ZmanimApi.js
@@ -6,29 +6,68 @@ let itsRoshHashana = false;
 let itsYomKippur = false;
 let yomTovim = [[{ isitChag: itsRoshHashana, chag: 'RoshaHashana' }], [{ isitChag: itsYomKippur, chag: 'yomkippur' }], [{ isitChag: itsSuccos, chag: 'Succos' }], [{ isitChag: itsPesach, chag: 'pesach' }], [{ isitChag: itsShavous, chag: 'Shavous' }]];
 let timeExtracted = '';
+var ArrowRight = document.getElementById('Arrow-Right');
+var ArrowLeft = document.getElementById('Arrow-Left');
+let i = 0
+let response = '';
+let Shabbos = '';
+var StartOfEvent = document.getElementById('Start-of-Event');
+var EndOfEvent = document.getElementById('End-Of-Event');
+let date = new Date();
+let finalStringDate = '';
+let goBack = false;
+let shabbosTimes = []
+finalStringDate += date.getFullYear() + '-'
+finalStringDate += (date.getMonth() + 1) > 9 ? (date.getMonth() + 1) : '0' + (date.getMonth() + 1) + '-';
+finalStringDate += date.getDate() + 1 > 9 ? date.getDate() : '0' + date.getDate();
+console.log(finalStringDate);
 
-async function HebDates() {
-    const response = await fetch(`https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&year=now&geo=city&city=US-Lakewood-NJ&d=on`);
-    const Shabbos = await response.json();
-    await ShabbosCode(Shabbos);
+ArrowRight.addEventListener('click', () => {
+    ShabbosCode(shabbosTimes);
+
+})
+ArrowLeft.addEventListener('click', () => {
+    if (i === 2) {
+        return;
+    }
+    i = i - 4;
+    console.log(i);
+    ShabbosCode(shabbosTimes);
+
+
+})
+ZmanFetch(finalStringDate);
+async function ZmanFetch() {
+    response = await fetch(`https://www.hebcal.com/hebcal?v=1&cfg=json&mf=off&start=${finalStringDate}&end=2023-12-01&geo=city&city=US-Lakewood-NJ`);
+    Shabbos = await response.json();
+    console.log(Shabbos);
+    for (let i = 0; i < Shabbos.items.length; i++) {
+        if (Shabbos.items[i].category === "candles" || Shabbos.items[i].category === "havdalah") {
+
+            shabbosTimes.push(Shabbos.items[i]);
+        }
+    }
+    console.log(shabbosTimes);
+    ShabbosCode(shabbosTimes);
 
 }
 function ShabbosCode(event) {
-    var StartOfEvent = document.getElementById('Start-of-Event');
-    var EndOfEvent = document.getElementById('End-of-Event');
 
     let YomTov = '';
     let Shabbos = '';
+    let NotFinshed = true;
+    let startMade = false;
+    while (NotFinshed === true) {
 
-    for (let i = 0; i < event.items.length; i++) {
-        if (event.items[i].category === "hebdate") {
+        if (event[i].category === "hebdate") {
 
             checkIfTodayIsYomTov(event.items[i]);
         }
-        if (event.items[i].category === "candles" || event.items[i].category === "havdalah") {
-            let time = new Date(event.items[i].date);
-            let now = new Date(event.items[i].date).toLocaleDateString('en-us', { weekday: "short", month: "long", day: "numeric" });
+        if (event[i].category === "candles" || event[i].category === "havdalah") {
 
+            let time = new Date(event[i].date);
+            let now = new Date(event[i].date).toLocaleDateString('en-us', { weekday: "short", month: "long", day: "numeric" });
+            console.log("hi");
             for (let i = 0; i < yomTovim.length; i++) {
                 if (yomTovim[i][0].isitChag === true) {
                     console.log(yomTovim[i][0].chag);
@@ -50,32 +89,25 @@ function ShabbosCode(event) {
                 Shabbos = '';
             }
             timeExtracted = `${now}, ${time.getHours() - 12}:${time.getMinutes() > 9 ? time.getMinutes() : `0` + time.getMinutes()}pm`;
-
-            calendarEl.innerHTML += timeExtracted;
-        }
-
-    }
-}
-function YomTovCode(event) {
-    var calendarEl = document.getElementById('calendar');
-    let YomTov = '';
-    let Shabbos = '';
-    checkIfTodayIsYomTov(event);
-    for (let i = 0; i < event.items.length; i++) {
-        let time = new Date(event.items[i].date);
-        let now = new Date(event.items[i].date).toLocaleDateString('en-us', { weekday: "short", month: "long", day: "numeric" });
-        for (let i = 0; i < yomTovim.length; i++) {
-            if (yomTovim[i][0].isitChag === true) {
-                console.log(yomTovim[i][0].chag);
-                event = yomTovim[i][0].chag;
-                break;
+            if (startMade === false) {
+                StartOfEvent.innerHTML = timeExtracted;
+                startMade = true;
+                console.log(timeExtracted + '  start');
             }
             else {
-                YomTov = '';
+                EndOfEvent.innerHTML = timeExtracted;
+                console.log(timeExtracted + '  end');
+                NotFinshed = false;
+
             }
 
         }
+        i++;
     }
+
+
+
+
 }
 function checkIfTodayIsYomTov(today) {
 
@@ -117,4 +149,3 @@ function checkIfTodayIsYomTov(today) {
     }
 
 }
-HebDates();
