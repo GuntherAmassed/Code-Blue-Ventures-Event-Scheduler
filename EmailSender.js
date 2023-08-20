3c89092e5b035a9c0e8577987495d033c50b640a
Moshe Stern <guntheramassed@gmail.com>
2023-08-20 13:32:27 -0400

    hello sir

--- EmailSender.js
+++ EmailSender.js
@@ -1,6 +1,14 @@
 const nodemailer = require('nodemailer');
 const fs = require('fs');
 let Html = fs.createReadStream(`Emailhopeitworks.html`);
+const port = 3100;
+const express = require('express');
+const cors = require('cors');
+cors.all;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -8,11 +16,11 @@ const transporter = nodemailer.createTransport({
         pass: 'ikmkqzbrovvznoyo'
     }
 });
+app.post('/NewUser',(res,req)=>{
 const mailOptions = {
     from: 'moshe@zunta.com',
-    to: 'moshe9012@gmail.com',
-    subject: '7',
-    text: 'That was easy!',
+    to: req.body.Email,
+    subject: 'Codeblue Times Sign up',
     html: Html,
     attachments: [{
         filename: 'CODEBLUE TIME.png',
@@ -33,3 +41,9 @@ transporter.sendMail(mailOptions, function (error, info) {
         console.log('Email sent: ' + info.response);
     }
 });
+
+})
+app.listen(port, () => {
+    console.log("listening on port " + port);
+})
+
