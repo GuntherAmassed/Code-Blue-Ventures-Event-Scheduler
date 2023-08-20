0335c55b2f3f0ffaa1ce1c2c054985186a2da8a6
Moshe Stern <guntheramassed@gmail.com>
2023-08-20 11:59:35 -0400

    hello

--- JsFiles/Mobile-Menu.js
+++ JsFiles/Mobile-Menu.js
@@ -2,21 +2,23 @@ var MobileFrame = document.getElementById("Menu-Mobile-Frame");
 var MobileFrameExtended = document.getElementById("Menu-Mobile-Frame-Extended");
 var OptionFrame = document.getElementById("Option-Frame");
 var ExIcon = document.getElementById("ExIcon");
+if (window.screen.width >= 1000) {
+    MobileFrame.style.display = "none";
+    MobileFrameExtended.style.display = "none";
+}
+if (window.screen.width < 999) {
+    MobileFrame.style.display = "flex";
+    MobileFrameExtended.style.display = "none";
+}
 OptionFrame.addEventListener('click', () => {
     MobileFrame.style.display = "none";
     MobileFrameExtended.style.display = "flex";
-    if (window.screen.width > 1000) {
-        MobileFrame.style.display = "none";
-        MobileFrameExtended.style.display = "none";
-    }
+
 })
 ExIcon.addEventListener('click', () => {
     MobileFrameExtended.style.display = "none";
     MobileFrame.style.display = "flex";
-    if (window.screen.width > 1000) {
-        MobileFrame.style.display = "none";
-        MobileFrameExtended.style.display = "none";
-    }
+
 
 })
 window.addEventListener('resize', () => {
